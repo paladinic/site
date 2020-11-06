9e212bc88af2314af80c7554fb244e0bffdc8961
Claudio Paladini# <clode.paladini@gmail.com>
2020-11-06 14:59:19 +0000

    major changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $(".header").css({
+      "background-color": "rgba(0,0,0,0.8)",
+      "font-size": "10px"
+});}
+  else {
+    $(".header").css({
+      "background-color": "black",
+      "font-size": "15px"
+    });
+  }
+}
