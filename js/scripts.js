7b0e8f25a3eb61cad271f1b20f45d34b992775c2
Claudio Paladini# <clode.paladini@gmail.com>
2020-11-13 15:22:15 +0000

    mobile friendly

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
       "background-color": "rgba(0,0,0,0.8)",
       "font-size": "10px"
