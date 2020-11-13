9af7893df842ed680f4a79d1cbb8de9b30f2c1f7
Claudio Paladini# <clode.paladini@gmail.com>
2020-11-13 15:44:58 +0000

    final mobile fixes

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
       "background-color": "rgba(0,0,0,0.8)",
-      "font-size": "10px"
+      "font-size": "0.6em"
 });}
   else {
     $(".header").css({
       "background-color": "black",
-      "font-size": "15px"
+      "font-size": "1em"
     });
   }
 }
