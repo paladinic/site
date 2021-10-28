57839959fe9ad63b885cd0d218c167331f585404
Claudio <clode.paladini@gmail.com>
2021-10-28 09:49:31 +0100

    restyle

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
-      "background-color": "rgba(0,0,0,0.8)",
       "font-size": "0.6em"
 });}
   else {
     $(".header").css({
-      "background-color": "black",
       "font-size": "1em"
     });
   }
