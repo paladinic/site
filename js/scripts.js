9154f07c913ba7c1183d1a5736a53abb5bd321d7
Claudio <clode.paladini@gmail.com>
2021-10-31 19:54:47 +0000

    restyle

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
-      "font-size": "0.6em"
+      "font-size": "0.6em",
+       'box-shadow':' 0px 0px 15px 3px rgba(0,0,0,1)'
 });}
   else {
     $(".header").css({
-      "font-size": "1em"
+      "font-size": "1em",
+       'box-shadow':' 0px 0px 8px 3px rgba(0,0,0,0)'
     });
   }
 }
