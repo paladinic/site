5630fc323e7ac2a50ea1dd228d613fa8e6f96145
Claudio <clode.paladini@gmail.com>
2022-04-28 22:47:07 +0100

    links and projects

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
-      "font-size": "0.6em",
-       'box-shadow':' 0px 0px 15px 3px rgba(255,255,255,1)'
+      "font-size": "0.6em"
 });}
   else {
     $(".header").css({
-      "font-size": "1em",
-       'box-shadow':' 0px 0px 8px 3px rgba(0,0,0,0)'
+      "font-size": "1em"
     });
   }
 }
