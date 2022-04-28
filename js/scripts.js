5a338d39aba77e82da6a01d86012ac41a8ce0336
Claudio <clode.paladini@gmail.com>
2022-04-28 18:59:31 +0100

    restyle

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     $(".header").css({
       "font-size": "0.6em",
-       'box-shadow':' 0px 0px 15px 3px rgba(0,0,0,1)'
+       'box-shadow':' 0px 0px 15px 3px rgba(255,255,255,1)'
 });}
   else {
     $(".header").css({
